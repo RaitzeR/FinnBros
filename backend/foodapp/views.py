f384ece0087affc2bda0e7ffb0652fb013c965ef
jussi <jussi.virta@vispal.fi>
2018-04-27 22:39:08 +0300

    Add CORS handling to outside requests

--- backend/foodapp/views.py
+++ backend/foodapp/views.py
@@ -4,6 +4,7 @@ from foodapp.models import FoodPost
 from django.core.serializers import serialize
 from django.http import HttpResponse, JsonResponse
 import json
+from urllib.parse import urlparse
 
 def index(request):
     return render(request, 'index.html', {
@@ -13,4 +14,14 @@ def index(request):
 def get_food_posts(request):
     food_posts = FoodPost.objects.all()
     food_posts_json = serialize('json', food_posts)
-    return JsonResponse(food_posts_json, safe=False)
+
+    jsonresp = JsonResponse(food_posts_json, safe=False)
+    try:
+        # If getting from outside heroku, get referrer_root and allow in CORS
+        parsed = urlparse(request.META['HTTP_REFERER'])
+        referrer_root = parsed.scheme + "://" + parsed.netloc
+        jsonresp['Access-Control-Allow-Origin'] = referrer_root
+    except KeyError:
+        pass
+
+    return jsonresp
