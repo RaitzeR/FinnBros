55ebde7eec3dda4f4bc1cf7e83aa214778e06899
jussi <jussi.virta@vispal.fi>
2018-04-27 21:53:58 +0300

    Add index.html

--- backend/foodapp/urls.py
+++ backend/foodapp/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from foodapp import views
+from django.urls import path
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
+    #url(r'^$', views.HomePageView.as_view()),
+    path('', views.index),
     url(r'^links/$' , views.LinksPageView.as_view()),
 ]
--- backend/foodapp/views.py
+++ backend/foodapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+def index(request):
+    return render(request, 'index.html', {
+
+    })
+
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
