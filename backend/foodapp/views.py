8dede6ebc3854b2e6de09b8fdfbc5bc228c78cef
jussi <jussi.virta@vispal.fi>
2018-04-27 23:05:40 +0300

    Improve api

--- backend/foodapp/views.py
+++ backend/foodapp/views.py
@@ -7,6 +7,15 @@ import json
 from urllib.parse import urlparse
 from django.db import IntegrityError
 
+def get_referrer_root(request):
+    try:
+        # If getting from outside heroku, get referrer_root and allow in CORS
+        parsed = urlparse(request.META['HTTP_REFERER'])
+        referrer_root = parsed.scheme + "://" + parsed.netloc
+    except KeyError:
+        referrer_root = ""
+    return referrer_root
+
 def index(request):
     return render(request, 'index.html', {
 
@@ -17,21 +26,21 @@ def get_food_posts(request):
     food_posts_json = serialize('json', food_posts)
 
     jsonresp = JsonResponse(food_posts_json, safe=False)
-    try:
-        # If getting from outside heroku, get referrer_root and allow in CORS
-        parsed = urlparse(request.META['HTTP_REFERER'])
-        referrer_root = parsed.scheme + "://" + parsed.netloc
-        jsonresp['Access-Control-Allow-Origin'] = referrer_root
-    except KeyError:
-        pass
+    jsonresp['Access-Control-Allow-Origin'] = get_referrer_root(request)
 
     return jsonresp
 
 def create_food_post(request):
     title = request.POST.get("title")
+
     try:
         new_food_post = FoodPost(title=title)
         new_food_post.save()
     except IntegrityError as e:
-        return JsonResponse({"message": e.args})
-    return HttpResponse(200)
+        resp = JsonResponse({"message": e.args})
+        resp['Access-Control-Allow-Origin'] = get_referrer_root(request)
+        return resp
+
+    resp = HttpResponse(200)
+    resp['Access-Control-Allow-Origin'] = get_referrer_root(request)
+    return resp
