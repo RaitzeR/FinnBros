592d0fa5ee740d22f8d580afeaa8406b4aebd598
Ville Virta <raitzer1@hotmail.com>
2018-04-29 15:49:36 +0200

    watson returns hard coded values

--- backend/foodapp/Vision.py
+++ backend/foodapp/Vision.py
@@ -1,5 +1,7 @@
 from modules.watson_developer_cloud import VisualRecognitionV3
 from operator import itemgetter
+from io import BytesIO, StringIO
+from PIL import Image
 import base64
 
 
@@ -15,28 +17,39 @@ class ImageClasses:
         self.classes = self.getClasses()
 
     def getClasses(self):
+        return [{"score":0.97,"class":"apple"},{"score":0.97,"class":"fruit"},{"score":0.97,"class":"accessory fruit"},{"score":0.97,"class":"dessert apple"}]
         vision = VisualRecognitionV3(
             api_key="55756fcdc3bc6672c581812037e7296e3d74f396",
             version="2018-03-19"
         )
+        decode_str = base64.b64decode(self.image_url.encode('ascii'))
+        file_like = BytesIO(decode_str)
+        img = Image.open(file_like)
+
 
         #if "http" not in self.image_url:
-        img = base64.decodestring(self.image_url.encode('ascii'))
+        #img = base64.decodestring(self.image_url.encode('ascii'))
         with open('static/images/test.jpg','wb') as fh:
             fh.write(base64.decodebytes(img))
 
-        with open('static/images/test.jpg', 'rb') as f:
-            response = vision.classify(images_file=f,
-                                       threshold=self.threshold,
-                                       accept_language="en",
-                                       classifier_ids=["food"],
-                                       owners=["IBM"])
-        #else:
-        #    response = vision.classify(url=self.image_url,
+            response = vision.classify(images_file=fh,
+                                   threshold=self.threshold,
+                                   accept_language="en",
+                                   classifier_ids=["food"],
+                                   owners=["IBM"])
+
+        #with open('static/images/test.jpg', 'rb') as f:
+        #    response = vision.classify(images_file=f,
         #                               threshold=self.threshold,
         #                               accept_language="en",
         #                               classifier_ids=["food"],
         #                               owners=["IBM"])
+        #else:
+            response = vision.classify(url=self.image_url,
+                                       threshold=self.threshold,
+                                       accept_language="en",
+                                       classifier_ids=["food"],
+                                       owners=["IBM"])
 
         dictionaryList = response["images"][0]["classifiers"][0]["classes"]
         return sorted(dictionaryList, key=itemgetter("score"), reverse=True)
